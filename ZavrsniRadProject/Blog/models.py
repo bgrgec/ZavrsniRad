bcc3b38a0e760030a21b30622f48fd5199866766
bgrgec <64777522+bgrgec@users.noreply.github.com>
2023-02-10 12:20:46 +0000

    Dodana klasa Post i image field, instaliran Pillow

--- ZavrsniRadProject/Blog/models.py
+++ ZavrsniRadProject/Blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Post(models.Model):
+    postName = models.CharField(max_length=120)
+    featuredImage = models.ImageField(upload_to='media/')
+    postContent = models.CharField(max_length=65535)
