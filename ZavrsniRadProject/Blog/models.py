daafdbe8c1c434bcae2b518f17cb391bb376c34f
bgrgec <64777522+bgrgec@users.noreply.github.com>
2023-02-10 11:02:39 +0100

    Update

--- ZavrsniRadProject/Blog/models.py
+++ ZavrsniRadProject/Blog/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    imeAutora = models.CharField(max_length=50)
+    active = models.BooleanField(default=False)
+
+
+class Category(models.Model):
+    nazivKategorije = models.CharField(max_length=100)
+    published = models.BooleanField(default=False)
+
+
+class Post(models.Model):
+    naslovObjave = models.CharField(max_length=200)
+    objavaContent = models.CharField(max_length=65535)
+    published = models.BooleanField(default=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
--- ZavrsniRadProject/Blog/views.py
+++ ZavrsniRadProject/Blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def index(request):
-    return render(request, 'templates/Blog/index.html')
+    return render(request, 'Blog/index.html')
 
 def singlePost(request):
     return render(request, 'templates/Blog/singlePost.html')
