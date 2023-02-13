e5fd77ebc65899652522934733d1d6885246e404
bgrgec <64777522+bgrgec@users.noreply.github.com>
2023-02-13 20:47:32 +0000

    CSS, static

--- ZavrsniRadProject/Blog/urls.py
+++ ZavrsniRadProject/Blog/urls.py
@@ -6,5 +6,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('', views.index, name='index'),
     path('about/', views.about, name='about'),
+    path('posts/', views.posts, name='posts'),
+    path('contact/', views.contact, name='contact'),
     path('post/<int:postID>/', views.singlePost, name='singlePost'),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_URL)  + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- ZavrsniRadProject/Blog/views.py
+++ ZavrsniRadProject/Blog/views.py
@@ -17,6 +17,18 @@ def singlePost(request, postID):
     
     return render(request, 'Blog/singlePost.html', context=context)
 
+
+def posts(request):
+    postList = Post.objects.all().order_by('-id')
+    context = {
+        'postData':postList,
+    }   
+    return(render(request, 'Blog/posts.html', context=context))
+
 def about(request):
     context={}
     return(render(request, 'Blog/about.html', context=context))
+
+def contact(request):
+    context={}
+    return(render(request, 'Blog/contact.html', context=context))
