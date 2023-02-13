e0d0b84f78f0c7e1eb0b8d679c3b4ea6cc8bdc28
bgrgec <64777522+bgrgec@users.noreply.github.com>
2023-02-13 09:23:44 +0000

    Homepage

--- ZavrsniRadProject/Blog/views.py
+++ ZavrsniRadProject/Blog/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from .models import Post
 # Create your views here.
 def index(request):
-    postList = Post.objects.all()
+    postList = Post.objects.all().order_by('-id')
     context = {
         'postData':postList,
     }
